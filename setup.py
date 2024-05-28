3a4276717bdbc5e3ae99acd6746c2bc32d28250b
brayanpadilla <brayanpadilla198@gmail.com>
2024-05-28 01:16:22 -0500

    Creacion de nodo para el movimiento de la base lidar fisica El nodo esta en la carpeta script, codigo.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'move_joint = scripts.move_joint:main',
+            'move_joint = scripts.codigo:main',
         ],
     },
 )
